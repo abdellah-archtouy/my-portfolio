757dbee5be57c2b13fffbc85a56170189791801e
Abdellah Archtouy <aarchtou@e1r11p2.1337.ma>
2024-12-12 15:17:10 +0100

    done

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,7 @@
 import '@fontsource/montserrat';
 import '@fontsource/raleway';
 import '@fontsource/open-sans';
+import Image from 'next/image';
 import { FaGithub } from "react-icons/fa6";
 import { CiAt } from "react-icons/ci";
 import { TbBrandLinkedinFilled } from "react-icons/tb";
@@ -10,7 +11,9 @@ export default function Home() {
       <div className=' overflow-x-hidden overflow-y-auto scrollbar-thin scrollbar-track-transparent scrollbar-thumb-transparent'>
       <div className='grid bg-black relative overflow-hidden w-screen h-screen items-end xl:bg-[#D7D7D7]'>
       <div className='absolute bg-[#D7D7D7] w-[200%] h-full  top-[70%] rotate-[-10deg]  xl:rotate-12 xl:top-[-50%] xl:h-[250%] xl:right-[70%] xl:bg-black '></div>
-        <img className="  w-auto h-[90%]  absolute bottom-[-5%] right-[-10%] xl:left-[10%] xl:h-[80%]  " src="/my-picture.png" alt="my image" />
+        <Image className="  w-auto h-[90%]  absolute bottom-[-5%] right-[-10%] xl:left-[10%] xl:h-[80%]  "
+         src="/my-picture.png" 
+         alt="my image" />
       <div className='absolute bg-[#0000006b] w-[200%] h-full  top-[70%] rotate-[-10deg] xl:hidden '></div>
       <div className='z-0 relative flex  w-full  h-[20%] xl:h-full xl:w-[60%] xl:gap-[5%] lg:pb-[0] xl:pb-[8%] lg:mb-7 xl:mb-0 xl:flex-col  justify-self-end xl:text-[#000000] pr-[3%] justify-end 2xl:justify-end xl:justify-center '>
         <nav className="xl:flex flex-row h-[10%] absolute top-[60px] right-[10%] 2xl:h-[15%] 2xl:w-[60%] w-[50%] hidden justify-end text-[clamp(0.75rem,0.639vw+0.341rem,1.875rem)]  text-[#0000009e] gap-[10%] justify-self-end m-0 self-end font-bold ">
@@ -27,7 +30,7 @@ export default function Home() {
         </div> 
         <div className='2xl:flex flex-col hidden  gap-[8%] 2xl:gap-[25px] '>
           <h2 className='leading-none font-raleway text-[20px] sm:text-[25px] md:text-[30px]  2xl:text-[250%] font-bold' >About Me:</h2>
-          <p className='font-open-sans font-normal lg:text-[16px] xl:text-[220%] 2xl:text-[220%]  ' >Hi, I'm Abdellah, a passionate Full-Stack Developer and UI Designer currently studying 
+          <p className='font-open-sans font-normal lg:text-[16px] xl:text-[220%] 2xl:text-[220%]  ' >Hi, I&#39;m Abdellah, a passionate Full-Stack Developer and UI Designer currently studying 
             at 1337 (42) School of Programming. I specialize in building intuitive,
              responsive, and visually appealing user interfaces, as well as developing robust back-end
               systems. My technical expertise spans across front-end technologies like React, HTML, CSS,
@@ -43,7 +46,7 @@ export default function Home() {
       <div className='w-screen h-screen bg-[#D7D7D7] flex flex-col px-[5%]  items-center xl:hidden gap-[10%] justify-center '>
         <h1 className='text-black font-bold font-raleway text-[30px]  '>About Me:</h1>
         <p className='font-open-sans text-black font-normal text-[16px] sm:text-[22px] md:text-[32px] xl:text-[220%] 2xl:text-[220%]  ' >
-          Hi, I'm Abdellah, a passionate Full-Stack Developer and UI Designer currently studying 
+          Hi, I&#39;m Abdellah, a passionate Full-Stack Developer and UI Designer currently studying 
             at 1337 (42) School of Programming. I specialize in building intuitive,
             responsive, and visually appealing user interfaces, as well as developing robust back-end
               systems. My technical expertise spans across front-end technologies like React, HTML, CSS,
